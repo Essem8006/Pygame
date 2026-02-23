1e3f16e07844731893a46261c76f8a8de4cfd7e9
Essem <149950536+Essem8006@users.noreply.github.com>
2026-02-23 19:32:04 +0000

    start redo of fluid

--- fluid/lightweight-python/grid.py
+++ fluid/lightweight-python/grid.py
@@ -1,4 +1,3 @@
-# to test the areas have 4 points
 
 import pygame
 import random
@@ -8,8 +7,12 @@ pygame.init()
 
 
 gravity = 9.81
-damping = 0.99
+damping = 0.001
 debug = True
+N = 10
+
+width = 500/(2*(N+1))
+line_scale = width - 1
 
 
 white = (255, 255, 255)
@@ -22,34 +25,34 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Fluid")
 
 
-
 class Point:
-    def __init__(self, x, y, size):
+    def __init__(self, x, y):
+        self.value = random.random()
         self.pos = [x*screen_width, y*screen_height]
-        self.vector = [0,0]
-    def update(self):
-        # apply gravity to move points
+        self.flow = [0,0,0,0]
 
-        # get collisions to mark some points as unable to expand
+    def update(self, surrounding):
+        for i in range(4):
+            self.flow[i] = max(self.value - surrounding[i], 0)
+        if self.value > 1:
+            print(self.value)
 
-        # calculate the size
-
-        # compare to target size
-
-        # expand as appropriate
-        
-        #self.draw()
-        pass
     def draw(self):
-        pygame.draw.circle(screen, (255, 255, 255, 255), (self.pos[0], self.pos[1]), 2)
+        pygame.draw.rect(screen, (0,255*self.value,0), [self.pos[0]-width, self.pos[1]-width, 2*width, 2*width])
+        pygame.draw.circle(screen, white, (self.pos[0], self.pos[1]), 2)
+        pygame.draw.line(screen, white, (self.pos[0], self.pos[1]), (self.pos[0], self.pos[1]+self.flow[0]*line_scale))
+        pygame.draw.line(screen, white, (self.pos[0], self.pos[1]), (self.pos[0]+self.flow[1]*line_scale, self.pos[1]))
+        pygame.draw.line(screen, white, (self.pos[0], self.pos[1]), (self.pos[0], self.pos[1]-self.flow[2]*line_scale))
+        pygame.draw.line(screen, white, (self.pos[0], self.pos[1]), (self.pos[0]-self.flow[3]*line_scale, self.pos[1]))
  
 running = True
 clock = pygame.time.Clock()
 
 points = []
-for i in range(10):
-    for j in range(10):
-        points.append(new Point( (i+1)*500/11 , (j+1)*500/11 ))
+for j in range(N):
+    points.append([])
+    for i in range(N):
+        points[j].append(Point( (i+1)/(N+1), (j+1)/(N+1) ))
 
 while running:
     #events
@@ -59,8 +62,61 @@ while running:
         elif event.type == pygame.WINDOWMOVED:
             pass # todo
 
-    for p in points:
-        a.draw()
+    pygame.draw.rect(screen, black, [0, 0, 500, 500])
+
+    # get flow
+    for y in range(N):
+        for x in range(N):
+            if y == 0:
+                up = points[y][x].value
+                down = points[y+1][x].value
+            elif y == N-1:
+                up = points[y-1][x].value
+                down = points[y][x].value
+            else:
+                up = points[y-1][x].value
+                down = points[y+1][x].value
+            
+            if x == 0:
+                left = points[y][x].value
+                right = points[y][x+1].value
+            elif x == N-1:
+                left = points[y][x-1].value
+                right = points[y][x].value
+            else:
+                left = points[y][x-1].value
+                right = points[y][x+1].value
+            
+            surround = [down, right, up, left]
+            points[y][x].update(surround)
+            points[y][x].draw()
+
+    # move water
+    for y in range(N):
+        for x in range(N):
+            if y == 0:
+                points[y+1][x].value += points[y][x].flow[0] * damping
+                points[y][x].value -= points[y][x].flow[0] * damping
+            elif y == N-1:
+                points[y-1][x].value += points[y][x].flow[2] * damping
+                points[y][x].value -= points[y][x].flow[2] * damping
+            else:
+                points[y+1][x].value += points[y][x].flow[0] * damping
+                points[y-1][x].value += points[y][x].flow[2] * damping
+                points[y][x].value -= points[y][x].flow[0] * damping
+                points[y][x].value -= points[y][x].flow[2] * damping
+            
+            if x == 0:
+                points[y][x+1].value += points[y][x].flow[1] * damping
+                points[y][x].value -= points[y][x].flow[1] * damping
+            elif x == N-1:
+                points[y][x-1].value += points[y][x].flow[3] * damping
+                points[y][x].value -= points[y][x].flow[3] * damping
+            else:
+                points[y][x+1].value += points[y][x].flow[1] * damping
+                points[y][x-1].value += points[y][x].flow[3] * damping
+                points[y][x].value -= points[y][x].flow[1] * damping
+                points[y][x].value -= points[y][x].flow[3] * damping
 
     pygame.display.flip()
     clock.tick(60)
